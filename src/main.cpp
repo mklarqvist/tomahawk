cca29e44d35c15804f7095f74be69ff5c0c219d2
Marcus D. R. Klarqvist <mk21@sanger.ac.uk>
2017-09-16 15:27:50 +0100

    trigger

--- src/main.cpp
+++ src/main.cpp
@@ -71,8 +71,6 @@ int main(int argc, char** argv){
 
 	} else if(strncmp(&argv[1][0], "stats", 5) == 0){
 		return(stats(argc, argv));
-		//std::cerr << "Not implemented" << std::endl;
-		//return(1);
 
 	} else if(strncmp(&argv[1][0], "--version", 9) == 0 || strncmp(&argv[1][0], "version", 7) == 0){
 		programMessage(false);
