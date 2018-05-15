5eadbdcb66b4b2aa3f866d63ca0c8cf6194b8cc6
mklarqvist <mk819@cam.ac.uk>
2018-05-15 08:39:05 +0100

    gitignore fix

---
+++ src/tomahawk/subdir.mk
@@ -0,0 +1,24 @@
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+src/tomahawk/TomahawkCalc.cpp \
+src/tomahawk/tomahawk_importer.cpp \
+src/tomahawk/tomahawk_reader.cpp \
+src/tomahawk/import_writer.cpp 
+
+OBJS += \
+src/tomahawk/TomahawkCalc.o \
+src/tomahawk/tomahawk_importer.o \
+src/tomahawk/tomahawk_reader.o \
+src/tomahawk/import_writer.o 
+
+CPP_DEPS += \
+src/tomahawk/TomahawkCalc.d \
+src/tomahawk/tomahawk_importer.d \
+src/tomahawk/tomahawk_reader.d \
+src/tomahawk/import_writer.d 
+
+# Each subdirectory must supply rules for building sources it contributes
+src/tomahawk/%.o: src/tomahawk/%.cpp
+	g++ -std=c++0x -I"src/" -O3 -march=native -mtune=native -ftree-vectorize -pipe -frename-registers -funroll-loops -c -DVERSION=\"$(GIT_VERSION)\" -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+
+
