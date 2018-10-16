153f2f2fc3c2102c958bbe7d8dbc46509efc2b55
mklarqvist <mk819@cam.ac.uk>
2018-10-16 16:08:12 +0100

    fix gitignore

---
+++ Debug/subdir.mk
@@ -0,0 +1,42 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../buffer.cpp \
+../ld.cpp \
+../main.cpp \
+../support_vcf.cpp \
+../utility.cpp \
+../vcf_utils.cpp \
+../zstd_codec.cpp 
+
+OBJS += \
+./buffer.o \
+./ld.o \
+./main.o \
+./support_vcf.o \
+./utility.o \
+./vcf_utils.o \
+./zstd_codec.o 
+
+CPP_DEPS += \
+./buffer.d \
+./ld.d \
+./main.d \
+./support_vcf.d \
+./utility.d \
+./vcf_utils.d \
+./zstd_codec.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -std=c++0x -I/usr/local/include/ -O3 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
